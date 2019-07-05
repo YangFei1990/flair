3cfe6230d52ebbd5409a085bd3b885c3bdbac643
Fei Wu <fewu@amazon.com>
2019-07-05 14:47:33 -0700

    add print

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -261,10 +261,16 @@ class ModelTrainer:
 
                 train_loss /= seen_batches
 
+                if horovod:
+                    from mpi4py import MPI
+                    comm = MPI.COMM_WORLD
+                    size = comm.Get_size()
+                    train_loss = comm.allreduce(train_loss, op=MPI.SUM) / size
+
                 self.model.eval()
 
                 log_line(log)
-                log.info(
+                if rank == 0: log.info(
                     f"rank:{rank}:EPOCH {epoch + 1} done: loss {train_loss:.4f} - lr {learning_rate:.4f}"
                 )
                 end = time.time()
